79cab3337b2943db4b0e50c06951c88b562abddc
nasalina120 <nasalina120@gmail.com>
2026-08-06 17:10:22 +0200

    Add push and unshift operations

--- Task1/array.js
+++ Task1/array.js
@@ -1,1 +1,10 @@
- 
+const arrNums = [1, 4, 8, -4, 4, 6, 0.4];
+console.log("Масив arrNums :>> ", arrNums);
+
+const elmentpush = +prompt("Input elmentpush:");
+arrNums.push(elmentpush);
+console.log("Масив arrNums + elmentpush:>> ", arrNums);
+
+const elmentunshift = +prompt("Input unshift:");
+arrNums.unshift(elmentunshift);
+console.log("Масив arrNums + elmentunshift :>> ", arrNums);
