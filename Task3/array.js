b3230f7df6c54ddb677ab63cb71058f05db27b0a
nasalina120 <nasalina120@gmail.com>
2026-08-08 14:52:05 +0200

    Refactor functions

--- Task3/array.js
+++ Task3/array.js
@@ -1,13 +1,6 @@
-// 3.4. Знайти в масиві перше число, яке є більшим за 50. Вивести це число в консоль.
-// Якщо такого числа немає — вивести undefined.
-
-//  3.4 - findIndex, 3.5 - find.
-
 const arrs = [-1, 5, 0, 9, -10];
 
-function nonZeroNums(numbers) {
-  return numbers !== 0;
-}
+const nonZeroNums = (numbers) => numbers !== 0;
 
 const arrNonZeros = arrs.filter(nonZeroNums);
 console.log("arrs :>> ", arrs);
@@ -15,21 +8,18 @@ console.log("arrNonZero :>> ", arrNonZeros);
 
 const arrs1 = [99, 5, 0, 10, 9, 30];
 
-function divideBy100(numbers) {
-  return numbers / 100;
-}
+const divideBy100 = (numbers) => numbers / 100;
+
 const divNums = arrs1.map(divideBy100);
 console.log("arrs1 :>> ", arrs1);
 console.log("divNums :>> ", divNums);
 
-function cubeNum(items) {
-  console.log("items :>> ", items ** 3);
-}
+const cubeNum = (items) => console.log("items :>> ", items ** 3);
+
 arrs1.forEach(cubeNum);
 
-function checkSquare100(num) {
-  return num ** 2 === 100;
-}
+const checkSquare100 = (num) => num ** 2 === 100;
+
 console.log("arrs1 :>> ", arrs1);
 
 const targetIndex = arrs1.findIndex(checkSquare100);
@@ -42,9 +32,7 @@ if (targetIndex !== -1) {
 
 console.log("arrs1 :>> ", arrs1);
 
-function isGreater50(num) {
-  return num > 50;
-}
+const isGreater50 = (num) => num > 50;
 
 const foundNum = arrs1.find(isGreater50);
 console.log("foundNum  :>> ", foundNum);
