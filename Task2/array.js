dc9b0f791af82f6727c741b974225d5e29ecb548
nasalina120 <nasalina120@gmail.com>
2026-08-08 13:17:40 +0200

    Calculate the multiplication result

--- Task2/array.js
+++ Task2/array.js
@@ -8,5 +8,12 @@ const arrNums = [1, 4, 8, -4, 4, 6, 0.4];
 console.log("Масив arrNums :>> ", arrNums);
 
 for (let i = 0; i < arrNums.length; i++) {
-  if (i % 2 === 0) console.log("arrNums[i] з парним  індексом:>> ", arrNums[i]);
+  if (i % 2 === 0)
+    console.log(`arrNums[i] з парним  індексом ${i}:>> `, arrNums[i]);
 }
+
+let multResult = 1;
+for (let i = 0; i < arrNums.length; i++) {
+  multResult *= arrNums[i];
+}
+console.log("multResult :>> ", multResult);
